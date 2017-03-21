1bdf4303c217e09ac590ea01aa1885caef29c564
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2017-03-20 23:23:17 -0300

    criação do primeiro modelo

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Minhas Applicações
+    'learning_logs',
 ]
 
 MIDDLEWARE = [
