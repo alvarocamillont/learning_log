1824bf64228dce655b3fc8eaf7dc24ba1971499c
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2017-03-24 23:42:41 -0300

    Incluido a pagina de tópicos em geral

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     # Mostra todos os assuntos
-    #url(r'^topics/$', views.topics, name='topics'),
+    url(r'^topics/$', views.topics, name='topics'),
 ]
