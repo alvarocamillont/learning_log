3f00d4085b2db5b053388c456e2b69050eedf8cf
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2017-03-24 23:22:16 -0300

    Herança de template

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     # Pagina Inicial
     url(r'^$', views.index, name='index'),
+
+    # Mostra todos os assuntos
+    #url(r'^topics/$', views.topics, name='topics'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -4,3 +4,4 @@ from django.shortcuts import render
 def index(request):
     """Página inicial da aplição"""
     return render(request, 'learning_logs/index.html')
+
