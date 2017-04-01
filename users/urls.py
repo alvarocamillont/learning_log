f6b644d61fb18ba003bbbf33a7ffe6abb893bec1
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2017-03-31 23:17:32 -0300

    Criado Logout

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # Página de login
     url(r'^login/$', login, {'template_name': 'users/login.html'},
         name='login'),
+    url(r'^logout/$', views.logout_view, name='logout'),
 ]
