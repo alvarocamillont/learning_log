92cf58296b91274ab0ba245dc93ad29d835b69f2
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2017-03-31 22:59:26 -0300

    Implementação da tela de Login

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Minhas Applicações
     'learning_logs',
+    'users',
 ]
 
 MIDDLEWARE = [
---
+++ users/urls.py
@@ -0,0 +1,11 @@
+"""Define padrão de urls para users"""
+from django.conf.urls import url
+from django.contrib.auth.views import login
+
+from . import views
+
+urlpatterns = [
+    # Página de login
+    url(r'^login/$', login, {'template_name': 'users/login.html'},
+        name='login'),
+]
