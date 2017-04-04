d3fb243398524a0daa13673ff8077cc8e1c6f8aa
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2017-04-03 22:55:11 -0300

    Protegendo a pagina da entrada do tópico

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import EntryForm, TopicForm
@@ -24,6 +24,10 @@ def topics(request):
 def topic(request, topic_id):
     """Mostra os detalhes de um tópico"""
     topic = Topic.objects.get(id=topic_id)
+    # Garante que o tópico é do usuário
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
