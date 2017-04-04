ef8ee20898c1ea6ed7aab4848edd6e1366218f62
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2017-04-03 22:50:46 -0300

    Alterando o Modelo e filtrando os topicos por usuário

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,4 +1,5 @@
 """Modelos da APP Learning Logs"""
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -7,6 +8,7 @@ class Topic(models.Model):
     """Um assunto sobre o qual o usuário está aprendendo"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User)
 
     def __str__(self):
         """Devolve uma representação em string do modelo"""
